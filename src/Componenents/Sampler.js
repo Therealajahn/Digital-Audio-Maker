cb4637b2a77dc6865c1b84ebb17c0cf0996e5c4f
Therealajahn <therealajahn@gmail.com>
2020-12-08 21:14:19 -0600

    finally got sample to play

--- src/Componenents/Sampler.js
+++ src/Componenents/Sampler.js
@@ -1,72 +1,43 @@
 import { useEffect } from 'react';
-import axios from 'axios';
-import cuckoo from './Samples/cuckoo.ogg'
+// import axios from 'axios';
+
 
 
 
 
 function Sampler() {
-    
-           
-            const AudioContext = window.AudioContext || window.webkitAudioContext;
-            const audioCtx = new AudioContext();
-           
-            const file = import('./Samples/cuckoo.ogg')
-            
-            fetch(file)
-            .then(fetch =>{
-             fetch.arrayBuffer()
-           })
-            
-            function getSampleFile(audioContext,url){
-              console.log('import(url)', import(url))
-             
-             fetch(url)
-             .then(response =>{
-                return response.arrayBuffer()
-             })
-             .then(arrayBuffer =>{
-              console.log("ab",arrayBuffer) 
-              return audioContext.decodeAudioData(arrayBuffer)
-              })
-            
-          }
-          // getSampleFile(audioCtx,file);
-          
-          //   const setupSampleFile = async() => {
-          //     const filePath = './Samples/cuckoo.ogg';
-          //     const sample = await getSampleFile(audioCtx, filePath);
-          //     return sample;
-          // }//
-          // setupSampleFile()
-            
-            
-        //       let playbackRate = 1;
-              
-              
-          
-      
-        //     const playSample = (audioContext, audioBuffer) => {
-        //       const sampleSource = audioContext.createBufferSource();
-        //       sampleSource.buffer = audioBuffer;
-        //       sampleSource.playBackRate.setValueAtTime(playbackRate, audioCtx.currentTime);
-        //       sampleSource.connect(audioContext.destination)
-        //       sampleSource.start();
-        //       return sampleSource;
-        //   }
-        //   let cuckoo;
-          
-          
-        //   .then((sample) => {
-        //     cuckoo = sample;
-        //   });
-          
-        //   playSample(AudioContext, cuckoo);
-      
-    
+  useEffect(()=>{  
+  const audioContext = new (AudioContext || window.webkitAudioContext)();
+    let sample;
+
+    import('./Samples/cuckoo.ogg')
+    .then(url => { 
+      //to get response from audiofile
+      fetch(url.default)                                         
+      .then(response =>{
+         //to get array data of audio file 
+        return response.arrayBuffer();
+      })
+      .then(buffer => {
+         //resamples array buffer data to AC's sampling rate
+        return audioContext.decodeAudioData(buffer);
+      })
+      .then(decoded => {
+        //to create an audio node for the audio
+        sample = audioContext.createBufferSource();
+        sample.buffer = decoded;
+        //to connect to speakers
+        // sample.connect(audioContext.destination);
+        //to start audio 
+        // sample.start();
+      })
+    })
+  },[]);   
+
     return(
         <div className="Sampler"></div>
     )
 }
+      
 
 export default Sampler;
